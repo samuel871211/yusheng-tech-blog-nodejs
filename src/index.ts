39da7caefacfa0cfb8f49613c8cbca6ef25db071
yusheng chen <samuel871211@gmail.com>
2025-03-02 22:32:38 +0800

    feat: 學習 Connection 跟 Keep-Alive

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import { createServer } from 'http';
-
-const httpServer = createServer();
+let connectionCount = 0;
+const httpServer = createServer().listen(5000);
 httpServer.on('request', (req, res) => {
-    console.log(req);
-    console.log(res);
+    // https://github.com/nodejs/node/blob/main/lib/_http_server.js
+    // nodeJS httpServer 會自動根據 req.headers.connection 來決定
+    // res.setHeader('Connection', 'close');
+
+    // 需自行設定
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ success: true }));
 });
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Keep-Alive
+httpServer.on('connection', (socket) => {
+    connectionCount += 1;
+    console.log('connection', connectionCount);
+    socket.on('close', (hadError) => console.log('socket close', { hadError }));
+});
+// httpServer.on("dropRequest", (req, socket) => {
+//     console.log("dropRequest", req, socket);
+// });
+// httpServer.on("upgrade", (req, socket, head) => {
+//     console.log("upgrade", req, socket, head);
+// });
 // httpServer.on("close", () => {
 //     console.log("close");
 // });
@@ -29,14 +46,3 @@ httpServer.on('request', (req, res) => {
 // httpServer.on("clientError", (err, socket) => {
 //     console.log("clientError", err, socket);
 // });
-httpServer.on("connect", (req, socket, head) => {
-    console.log("connect", req, socket, head);
-    socket.end(JSON.stringify({ success: true }));
-});
-// httpServer.on("dropRequest", (req, socket) => {
-//     console.log("dropRequest", req, socket);
-// });
-// httpServer.on("upgrade", (req, socket, head) => {
-//     console.log("upgrade", req, socket, head);
-// });
-httpServer.listen(5000);
