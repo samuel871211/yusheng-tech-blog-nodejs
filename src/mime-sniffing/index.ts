33a6dda93d3156709f643ed2a1f882d4071ca94a
yusheng chen <samuel871211@gmail.com>
2025-04-26 19:43:50 +0800

    feat: mime-sniffing 新增 `X-Content-Type-Options`

--- src/mime-sniffing/index.ts
+++ src/mime-sniffing/index.ts
@@ -7,6 +7,7 @@ const image = readFileSync(join(__dirname, 'image.jpg'));
 httpServer.on('request', function requestListener (req, res) {
   if (req.url === "/favicon.ico") return faviconListener(req, res);
   res.setHeader("Content-Type", "text/plain");
-  console.log(image.buffer.slice(0, 4));
+  // turn on 'nosniff' mode
+  res.setHeader("X-Content-Type-Options", "nosniff");
   res.end(image);
 });
