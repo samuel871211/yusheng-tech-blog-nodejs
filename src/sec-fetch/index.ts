8e1d560cd2009b8a9cc3da1a7b7c9ddde1e1494b
yusheng chen <samuel871211@gmail.com>
2025-05-11 22:07:30 +0800

    feat: 新增 sec-fetch

---
+++ src/sec-fetch/index.ts
@@ -0,0 +1,7 @@
+import httpServer from "../httpServer";
+
+// todo-yusheng 研究要怎麼防範資安漏洞
+httpServer.on('request', function requestListener(req, res) {
+  res.setHeader("content-type", "application/json");
+  res.end(JSON.stringify(req.headers));
+})
