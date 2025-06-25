fed0b74f9814c642d77dd09c8b697f56a3a69c5b
yusheng <samuel871211@gmail.com>
2025-06-25 16:55:25 +0800

    feat: refresh 持續進行中

--- src/refresh/index.ts
+++ src/refresh/index.ts
@@ -4,12 +4,24 @@ import { faviconListener } from "../listeners/faviconListener";
 import { notFoundListener } from "../listeners/notFoundlistener";
 import { join } from "path";
 
-const beforeunload = readFileSync(join(__dirname, "beforeunload.html"));
-const metaRefresh = readFileSync(join(__dirname, "metaRefresh.html"));
+const beforeunloadHTML = readFileSync(join(__dirname, "beforeunload.html"));
+const metaRefreshHTML = readFileSync(join(__dirname, "metaRefresh.html"));
+const javascriptRedirectHTML = readFileSync(
+  join(__dirname, "javascriptRedirect.html"),
+);
 
 httpServer.removeAllListeners("request");
 httpServer.on("request", function requestListener(req, res) {
   if (req.url === "/favicon.ico") return faviconListener(req, res);
+
+  // Refresh without url
+  if (req.url === "/refresh-without-url") {
+    res.setHeader("Refresh", "3");
+    res.setHeader("Content-Type", "text/html");
+    res.end(`<h1>Your Request ID: ${crypto.randomUUID()}</h1>`);
+    return;
+  }
+  // referrer
   if (req.url === "/") {
     res.setHeader("Refresh", "3; url=http://localhost:5000/refreshed");
     res.setHeader("Content-Type", "text/html");
@@ -30,6 +42,42 @@ httpServer.on("request", function requestListener(req, res) {
         </body>
       </html>
     `);
+    return;
+  }
+  // Refresh with javascript protocol
+  if (req.url === "/javascript-protocol") {
+    res.setHeader("Refresh", "0; url=javascript:alert(1)");
+    res.setHeader("Content-Type", "text/html");
+    res.end(`<h1>Your Request ID: ${crypto.randomUUID()}</h1>`);
+    return;
+  }
+  // HTTP redirect vs refresh
+  if (req.url === "/http-redirect-vs-refresh") {
+    res.statusCode = 301;
+    res.setHeader("Location", "https://www.google.com");
+    res.setHeader(
+      "Refresh",
+      "0; url=https://developer.mozilla.org/zh-TW/docs/Web/HTTP/Reference/Status/301",
+    );
+    res.end();
+    return;
+  }
+  // JavsScript redirect vs refresh
+  if (req.url === "/javascript-redirect-vs-refresh") {
+    res.setHeader("Refresh", "0; url=https://developer.mozilla.org");
+    res.end(javascriptRedirectHTML);
+    return;
+  }
+  // meta refresh vs refresh
+  if (req.url === "/meta-refresh-vs-refresh") {
+    res.setHeader("Refresh", "0; url=https://developer.mozilla.org");
+    res.end(metaRefreshHTML);
+    return;
+  }
+  // beforeunload
+  if (req.url === "/beforeunload") {
+    res.setHeader("Refresh", "3; url=https://www.google.com");
+    res.end(beforeunloadHTML);
     return;
   }
   return notFoundListener(req, res);
