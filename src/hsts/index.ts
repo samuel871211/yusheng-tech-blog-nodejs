e75f88a5f56a896e4a60fac4d0c0568b21a077ce
yusheng chen <samuel871211@gmail.com>
2025-06-26 22:08:32 +0800

    feat: HSTS

--- src/hsts/index.ts
+++ src/hsts/index.ts
@@ -4,20 +4,23 @@ import { notFoundListener } from "../listeners/notFoundlistener";
 
 httpServer.removeAllListeners("request");
 httpServer.on("request", function httpRequestListener(req, res) {
+  console.log("receive request from https server port 5001");
   res.setHeader(
     "Strict-Transport-Security",
     "max-age=31536000; includeSubDomains",
   );
-  res.end();
-  return;
+  if (req.url === "/") return res.end();
+  return notFoundListener(req, res);
 });
 
 httpsServer.removeAllListeners("request");
 httpsServer.on("request", function httpsRequestListener(req, res) {
-  res.setHeader(
-    "Strict-Transport-Security",
-    "max-age=31536000; includeSubDomains",
-  );
+  // res.setHeader(
+  //   "Strict-Transport-Security",
+  //   "max-age=31536000; includeSubDomains",
+  // );
+
+  res.setHeader("Strict-Transport-Security", "max-age=0");
   if (req.url === "/") return res.end();
   return notFoundListener(req, res);
 });
--- src/https-server/README.md
+++ src/https-server/README.md
@@ -7,11 +7,20 @@ cert.pem
 
 How to generate these files with trusted CA?
 
+Mac
+
 ```zsh
 brew install mkcert
 brew install nss # to work on firefox
 mkcert -install
 mkcert -key-file private-key.pem -cert-file cert.pem localhost
+```
+
+Windows
+
+```powershell
+choco install mkcert
+# choco install nss 會失敗，但反正先不用 firefox 測試
 ```
 
 How to generate these files with untrusted CA?
