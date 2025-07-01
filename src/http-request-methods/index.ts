054c4c4ca1118ab3139a6d0f22fc5a3121040f16
yusheng chen <samuel871211@gmail.com>
2025-07-01 22:06:48 +0800

    feat: trace

--- src/http-request-methods/index.ts
+++ src/http-request-methods/index.ts
@@ -6,8 +6,13 @@ import { createConnection } from "net";
 
 httpServer.on("request", function requestListener(req, res) {
   if (req.url === "/favicon.ico") return faviconListener(req, res);
+  // test HEAD Request
   if (req.url === "/example.txt") {
     return send(req, String(req.url), { root: __dirname }).pipe(res);
+  }
+  // todo-yus
+  if (req.method === "TRACE" && req.url === "/TRACE") {
+    return res.end("ok");
   }
   return notFoundListener(req, res);
 });
