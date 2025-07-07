42351c7df45581305388680452e991c87eaa6bff
yusheng chen <samuel871211@gmail.com>
2025-07-07 22:21:20 +0800

    feat: cors ing

--- src/cross-origin-resource-sharing/index.ts
+++ src/cross-origin-resource-sharing/index.ts
@@ -29,5 +29,38 @@ http5001Server.on("request", function requestListener(req, res) {
     return;
   }
 
+  if (req.url === "/access-control-expose-headers") {
+    res.writeHead(200, {
+      "access-control-allow-origin": "http://localhost:5000",
+      "cache-control": "cache-control",
+      "content-language": "content-language",
+      "content-length": 0,
+      "content-type": "text/html",
+      expires: "expires",
+      "last-modified": "last-modified",
+      pragma: "pragma",
+      "access-control-expose-headers": "connection, date, keep-alive",
+    });
+    res.end();
+    return;
+  }
+
+  if (req.url === "/access-control-allow-headers") {
+    if (req.method === "OPTIONS") {
+      res.writeHead(204, {
+        "access-control-allow-origin": "http://localhost:5000",
+        "access-control-allow-headers": "authorization",
+      });
+      res.end();
+      return;
+    }
+    res.writeHead(200, {
+      "access-control-allow-origin": "http://localhost:5000",
+      "access-control-allow-headers": "authorization",
+    });
+    res.end();
+    return;
+  }
+
   return notFoundListener(req, res);
 });
