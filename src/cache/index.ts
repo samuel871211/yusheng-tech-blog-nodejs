f4deaf7740ffc4f49e0a9df35cba08dca632d78a
yusheng <samuel871211@gmail.com>
2025-07-14 16:58:25 +0800

    feat: cache 進行中

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -5,16 +5,20 @@ import { notFoundListener } from "../listeners/notFoundlistener";
 
 httpServer.on("request", function requestListener(req, res) {
   if (req.url === "/favicon.ico") return faviconListener(req, res);
-  if (req.url === "/image.jpg") {
+  if (req.url === "/image.jpg" || req.url === "/example.txt") {
     console.log(req.url);
-    return send(req, String(req.url), {
+    const sendStream = send(req, String(req.url), {
       root: __dirname,
-      cacheControl: true,
+      // cacheControl: true,
+      // immutable: true,
+      // maxAge: 1000,
       etag: true,
-      immutable: true,
       lastModified: true,
-      maxAge: 60000,
-    }).pipe(res);
+      cacheControl: false,
+    });
+    res.setHeader("Cache-Control", "public, max-age=5, must-revalidate");
+    sendStream.pipe(res);
+    return;
   }
   return notFoundListener(req, res);
 });
