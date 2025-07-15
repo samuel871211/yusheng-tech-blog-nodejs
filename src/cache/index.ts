d1100b519a05c250150dcf62ad98460dae1adc08
yusheng chen <samuel871211@gmail.com>
2025-07-15 22:25:09 +0800

    feat: cache ing

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -3,14 +3,15 @@ import httpServer from "../httpServer";
 import { faviconListener } from "../listeners/faviconListener";
 import { notFoundListener } from "../listeners/notFoundlistener";
 
+httpServer.removeAllListeners("request");
 httpServer.on("request", function requestListener(req, res) {
   const url = new URL(`http://localhost:5000${req.url}`);
   const qsCase = url.searchParams.get("case") || "1";
   if (url.pathname === "/favicon.ico") return faviconListener(req, res);
   if (url.pathname === "/image.jpg" || url.pathname === "/example.txt") {
     // 印出訊息
-    console.log(req.url, req.headers);
-
+    console.log("Request url:", req.url);
+    console.log("Request headers:", req.headers);
     // Last-Modified + If-Modified-Since
     if (qsCase === "1") {
       const sendStream = send(req, url.pathname, {
@@ -20,6 +21,10 @@ httpServer.on("request", function requestListener(req, res) {
         cacheControl: true,
         maxAge: 5000,
         immutable: true,
+      });
+      res.once("finish", () => {
+        console.log("Response headers:", res.getHeaders());
+        console.log("Status code:", res.statusCode);
       });
       sendStream.pipe(res);
       return;
