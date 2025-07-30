da83b8a40aa66898e82663005b931eb389ba3558
yusheng <samuel871211@gmail.com>
2025-07-30 16:47:16 +0800

    feat: smtp-server 改成 js
    feat: add socket-smtp-client

--- src/smtp-server/index.js
+++ src/smtp-server/index.js
@@ -1,18 +1,13 @@
-import { SMTPServer } from "smtp-server";
+const { SMTPServer } = require("smtp-server");
 
 const SMTPServerInstance = new SMTPServer({
-  name: "\r\nGET 123",
+  name: "smtp-server 3.14.0",
   logger: true,
   banner: "banner message",
   authMethods: ["LOGIN"],
-  enableTrace: true,
   secure: false,
   needsUpgrade: false,
-  // @ts-ignore
-  hideSize: false,
-  hideENHANCEDSTATUSCODES: false,
   hideSTARTTLS: true,
-  size: 0,
   onAuth(auth, session, callback) {
     console.log({ auth, session });
     callback(null, { user: { name: auth.username, password: auth.password } });
