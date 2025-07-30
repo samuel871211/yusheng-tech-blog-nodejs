dc151adbdc96914776e713dc3f2269a26c79064f
yusheng <samuel871211@gmail.com>
2025-07-30 08:03:47 +0800

    feat: smtp-server ing

--- src/smtp-server/index.ts
+++ src/smtp-server/index.ts
@@ -1,13 +1,22 @@
 import { SMTPServer } from "smtp-server";
 
 const SMTPServerInstance = new SMTPServer({
-  name: "yus test localhost SMTP Server",
+  name: "\r\nGET 123",
   logger: true,
   banner: "banner message",
+  authMethods: ["LOGIN"],
   enableTrace: true,
   secure: false,
   needsUpgrade: false,
-  // hideSTARTTLS: true
+  // @ts-ignore
+  hideSize: false,
+  hideENHANCEDSTATUSCODES: false,
+  hideSTARTTLS: true,
+  size: 0,
+  onAuth(auth, session, callback) {
+    console.log({ auth, session });
+    callback(null, { user: { name: auth.username, password: auth.password } });
+  },
 });
 
 SMTPServerInstance.listen(25);
