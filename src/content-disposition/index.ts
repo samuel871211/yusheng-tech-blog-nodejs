66a4c493c63f1bd1062acaad124afae6e8deec42
yusheng <samuel871211@gmail.com>
2025-11-14 16:27:20 +0800

    Content-Disposition ++

--- src/content-disposition/index.ts
+++ src/content-disposition/index.ts
@@ -9,9 +9,21 @@
 import httpServer from "../httpServer";
 import { readFileSync } from "fs";
 import { join } from "path";
+import { createServer } from "http";
 const image = readFileSync(join(__dirname, "image.jpg"));
 httpServer.on("request", function requestListener(req, res) {
   res.setHeader("Content-Type", "image/jpeg");
-  res.setHeader("Content-Disposition", "attachment; filename=image.jpg");
+  res.setHeader("Content-Disposition", "attachment");
+  // res.setHeader("Content-Disposition", "attachment; filename=image.jpg");
+  // res.setHeader("Content-Disposition", "inline");
   res.end(image);
 });
+
+createServer()
+  .listen(5001)
+  .on("request", (req, res) => {
+    res.setHeader("content-type", "text/html");
+    res.end(
+      '<a href="http://localhost:5000/image.jpg" target="_blank">hello</a>',
+    );
+  });
