18c01690976811ff48c3cc430c05b3c81eca999e
yusheng <samuel871211@gmail.com>
2025-11-18 15:36:23 +0800

    content-disposition ok

--- src/content-disposition/index.ts
+++ src/content-disposition/index.ts
@@ -32,17 +32,61 @@ httpServer.on("request", function requestListener(req, res) {
     // return;
 
     // attachment + 404 + `<a download>`
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Content-Disposition", "attachment; filename=test.html");
-    res.end("Only11Chars");
-    return;
+    // res.statusCode = 404;
+    // res.setHeader("Content-Type", "text/html");
+    // res.setHeader("Content-Disposition", "attachment; filename=test.html");
+    // res.end("Only11Chars");
+    // return;
 
     // Simple case
     // res.setHeader("Content-Type", "image/jpeg");
     // res.setHeader("Content-Disposition", "attachment; filename=image.jpg");
     // res.end(image);
     // return;
+
+    // filename=中文.jpg
+    // TypeError: Invalid character in header content ["Content-Disposition"]
+    // res.setHeader("Content-Type", "image/jpeg");
+    // res.setHeader("Content-Disposition", "attachment; filename=中文.jpg");
+    // res.end(image);
+    // return;
+
+    // filename=中文.jpg
+    // res.socket?.write(`HTTP/1.1 200 Ok\r\n`);
+    // res.socket?.write(`Content-Length: ${Buffer.byteLength(image)}\r\n`);
+    // res.socket?.write(`Content-Disposition: attachment; filename=中文.jpg\r\n`);
+    // res.socket?.write(`Content-Type: image/jpeg\r\n\r\n`);
+    // res.socket?.end(image);
+
+    // filename=hello world.jpg
+    // res.setHeader("Content-Type", "image/jpeg");
+    // res.setHeader("Content-Disposition", "attachment; filename=hello world.jpg");
+    // res.end(image);
+    // return;
+
+    // filename=hello%0D%0Aworld.jpg
+    // res.setHeader("Content-Type", "image/jpeg");
+    // res.setHeader("Content-Disposition", "attachment; filename=hello%0D%0Aworld.jpg");
+    // res.end(image);
+    // return;
+
+    // filename*=UTF8''%E4%B8%AD%E6%96%87.jpg
+    res.setHeader("Content-Type", "image/jpeg");
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename*=UTF8''%E4%B8%AD%E6%96%87.jpg",
+    );
+    res.end(image);
+    return;
+  }
+
+  if (req.url === "/ISO-8859-1") {
+    const buffer = Buffer.from("中文", "utf8");
+    res.setHeader("Content-Type", "text/html; charset=iso-8859-1");
+    res.setHeader("Content-Length", buffer.byteLength);
+    res.flushHeaders();
+    res.socket?.end(buffer);
+    return;
   }
 
   if (req.url === "/") {
