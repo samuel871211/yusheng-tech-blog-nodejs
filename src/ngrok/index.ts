c585e662134fd759d76f572d13cb1de0227f507b
yusheng <samuel871211@gmail.com>
2025-12-18 11:06:00 +0800

    ngrok ing

--- src/ngrok/index.ts
+++ src/ngrok/index.ts
@@ -1,6 +1,13 @@
+import { readFileSync } from "node:fs";
 import httpServer from "../httpServer";
+import { join } from "node:path";
 
 httpServer.on("request", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.end("<script>alert(1)</script>");
+  const url = new URL(req.url || "", "http://localhost:5000");
+  // res.setHeader("Content-Type", "text/html");
+  // res.end(readFileSync(join(__dirname, "index.html")));
+
+  res.statusCode = 302;
+  res.setHeader("Location", "https://www.google.com");
+  res.end();
 });
